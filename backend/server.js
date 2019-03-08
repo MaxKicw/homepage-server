917824bfa325f2675d932ec032c456666d9fa9ee
MaxKicw <m142679@googlemail.com>
2019-03-08 15:39:46 +0100

    Added Automation

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+//Endpoints
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post("/github", function (req, res) {
+  var sender = req.body.sender;
+  var branch = req.body.ref;
+
+  if(branch.indexOf('master') > -1 && sender.login === githubUsername){
+      deploy(res);
+  }
+})
 
 app.get('/api', function (req, res) {
   console.log("Tryed Connection");
@@ -16,3 +26,16 @@ app.get('/api', function (req, res) {
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
 });
+
+//Functions
+
+
+function deploy(res){
+  childProcess.exec('cd /home/max && ./deploy.sh', function(err, stdout, stderr){
+      if (err) {
+       console.error(err);
+       return res.send(500);
+      }
+      res.send(200);
+    });
+}
