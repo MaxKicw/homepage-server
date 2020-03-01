ba3fc3080640286ecad7ff0348c5a2d22c80b897
MaxKicw <m142679@googlemail.com>
2020-03-01 18:16:58 +0100

    Clean Start

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var GithubWebHook = require('express-github-webhook');
-var webhookHandler = GithubWebHook({ path: '/github', secret: 'nosecret' });
+// var GithubWebHook = require('express-github-webhook');
+// var webhookHandler = GithubWebHook({ path: '/github', secret: 'nosecret' });
 var exec = require('child_process').exec;
 var app = express();
 app.use(bodyParser.json()); // must use bodyParser in express
 
-app.use(webhookHandler); // use our middleware
+// app.use(webhookHandler); // use our middleware
 
 // Webhook 
 
-webhookHandler.on('*', function (event, repo, data) {
-  console.log("Jawoollll");
-  exec('cd ~/deploy/ && ./deploy.sh');
-});
+// webhookHandler.on('*', function (event, repo, data) {
+//   console.log("Jawoollll");
+//   exec('cd ~/deploy/ && ./deploy.sh');
+// });
 
 //Endpoints
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
 app.post("/githubkkkk", function (req, res) {
   console.log("Connection from Webhook");
   let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
@@ -32,6 +31,7 @@ app.post("/githubkkkk", function (req, res) {
 
 app.get('/api', function (req, res) {
   console.log("Tryed Connection");
+  console.log(req);
   res.setHeader('Access-Control-Allow-Origin',"https://www.maximilian-wick.de");
   res.setHeader('Access-Control-Allow-Methods',"GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.send({text:'Hi from the API'});
