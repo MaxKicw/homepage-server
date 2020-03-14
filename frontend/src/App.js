89c4091d05e20630debb4fca630d949dd2a47155
MaxKicw <m142679@googlemail.com>
2020-03-14 12:38:50 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome
+          Testing the webhook
         </p>
         <a
           className="App-link"
