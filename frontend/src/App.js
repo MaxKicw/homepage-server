8f5f081b6d8b278e33839e6039c21c3d2c0503e9
MaxKicw <m142679@googlemail.com>
2020-03-14 15:27:55 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 
+          Maxl mein Mitbewohner ist dumm wie Brot 
         </p>
         <a
           className="App-link"
