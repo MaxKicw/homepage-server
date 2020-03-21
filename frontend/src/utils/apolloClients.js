68f4dc48daffe646fa5f2af511db5aaa0d252b34
MaxKicw <m142679@googlemail.com>
2020-03-21 09:43:13 +0100

    Changed Apollo Path

--- frontend/src/utils/apolloClients.js
+++ frontend/src/utils/apolloClients.js
@@ -3,8 +3,7 @@ import  ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
     // uri: '${process.env.REACT_APP_BACKEND_URL}/graphql' || "http://localhost:1337/graphql"
-    uri:"http://localhost:1337/graphql"
-
+    uri:"https://maximilian-wick.de/graphql"
 });
 
 export default client;
