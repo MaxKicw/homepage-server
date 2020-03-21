eaf74b5bd563a3668b196e07dec6961e43be8526
MaxKicw <m142679@googlemail.com>
2020-03-21 18:33:51 +0100

    Responsive

--- frontend/src/components/Creditcard/Creditcard.js
+++ frontend/src/components/Creditcard/Creditcard.js
@@ -6,20 +6,19 @@ class Creditcard extends Component {
     state = {  }
     render() { 
         return ( 
-            <Pane
-                width="40vw"
-                className="Creditcard"
-                height="30vw"
-                maxHeight="200px"
-                maxWidth="400px"
-                borderRadius="10px"
-                backgroundColor="#FAE3CD"
-                elevation={3}
-                hoverElevation={4}
-                float="left"
-                marginLeft="5vw"
-                marginTop="15vh">
-            </Pane>
+            <div className="Creditcard">
+                <Pane
+                    position="relative"
+                    height="100%"
+                    left="5%"
+                    width="90%"
+                    borderRadius="10px"
+                    backgroundColor="#FAE3CD"
+                    elevation={3}
+                    hoverElevation={4}
+                    float="left">
+                </Pane>
+            </div>
          );
     }
 }
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -11,50 +11,49 @@ class Login extends Component{
     render(){
         return(
             <div className="Login">
-            <Pane
-                background="tint1"
-                float="left"
-                border="muted"
-                width="50vw"
-                height="100vh"
-                className="Login"
-                >
-                    <Pane 
-                        height="30vh"
-                        width="80%"
-                        marginTop="30vh"
-                        marginLeft="10%">
-                            <Heading 
-                                size={800}
-                                float="left"
-                                marginBottom="5vh"
-                                >
-                                Willkommen</Heading>
-                            <TextInputField
-                            placeholder="Nutzername"
-                            onChange={e => this.setState({ name: e.target.value })}
-                            />
-                            <TextInputField
-                            placeholder="Passwort"
-                            onChange={e => this.setState({ password: e.target.value })}
-                            type="password"
-                            />
-                            <Combobox
-                                items={['Private', 'Business']}
-                                width="50%"
-                                float="left"
-                                placeholder="Private"
-                                autocompleteProps={{
-                                    // Used for the title in the autocomplete.
-                                    title: 'Version'
-                                }}
-                                onChange={selected =>  this.setState({version:selected})}
-                            />
-                            <Button intent="success" float="right" iconBefore="tick-circle">Login</Button>
+                <Pane
+                    background="tint1"
+                    float="left"
+                    border="muted"
+                    width="100%"
+                    height="100vh"
+                    >
+                        <Pane 
+                            height="30vh"
+                            width="80%"
+                            marginTop="30vh"
+                            marginLeft="10%">
+                                <Heading 
+                                    size={800}
+                                    float="left"
+                                    marginBottom="5vh"
+                                    >
+                                    Willkommen</Heading>
+                                <TextInputField
+                                placeholder="Nutzername"
+                                onChange={e => this.setState({ name: e.target.value })}
+                                />
+                                <TextInputField
+                                placeholder="Passwort"
+                                onChange={e => this.setState({ password: e.target.value })}
+                                type="password"
+                                />
+                                <Combobox
+                                    items={['Private', 'Business']}
+                                    width="50%"
+                                    float="left"
+                                    placeholder="Private"
+                                    autocompleteProps={{
+                                        // Used for the title in the autocomplete.
+                                        title: 'Version'
+                                    }}
+                                    onChange={selected =>  this.setState({version:selected})}
+                                />
+                                <Button intent="success" float="right" iconBefore="tick-circle">Login</Button>
 
-                    </Pane>
-            </Pane>
-        </div>
+                        </Pane>
+                </Pane>
+            </div>
         );
     }
 }
