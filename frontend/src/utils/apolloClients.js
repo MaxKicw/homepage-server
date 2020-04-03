30bae07142cdd5d025ae392456ed76034b4f112e
MaxKicw <m142679@googlemail.com>
2020-04-03 16:37:38 +0200

    Fixed some dummheiten :D

--- frontend/src/utils/apolloClients.js
+++ frontend/src/utils/apolloClients.js
@@ -3,8 +3,8 @@ import  ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
     // uri: '${process.env.REACT_APP_BACKEND_URL}/graphql' || "http://localhost:1337/graphql"
-    // uri:"https://maximilian-wick.de/graphql"
-    uri:"http://localhost:1337/graphql"
+    uri:"https://maximilian-wick.de/graphql"
+    // uri:"http://localhost:1337/graphql"
 });
 
 export default client;
